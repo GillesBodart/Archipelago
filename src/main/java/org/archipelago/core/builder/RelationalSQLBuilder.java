f2b73e2ebb109a8ac73af902ed43b770eada10f5
Bodart Gilles <ABM589@win.corp.com>
2016-10-29 18:04:51 +0200

    First draft of table generated

--- src/main/java/org/archipelago/core/builder/RelationalSQLBuilder.java
+++ src/main/java/org/archipelago/core/builder/RelationalSQLBuilder.java
@@ -2,8 +2,6 @@ package org.archipelago.core.builder;
 
 import java.lang.reflect.Field;
 import java.sql.JDBCType;
-import java.sql.SQLData;
-import java.sql.SQLType;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -14,8 +12,6 @@ import org.archipelago.core.annotations.Island;
 import org.archipelago.core.domain.GeneratedScript;
 import org.archipelago.core.util.ArchipelagoUtils;
 import org.archipelago.core.util.StringTemplateFactory;
-import org.hibernate.Hibernate;
-import org.hibernate.boot.model.source.internal.hbm.HibernateTypeSourceImpl;
 import org.hibernate.type.descriptor.sql.JdbcTypeJavaClassMappings;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
@@ -37,7 +33,9 @@ public class RelationalSQLBuilder extends ArchipelagoScriptBuilder {
     @Override
     public List<GeneratedScript> makeScript(Class<?> clazz, List<Class<?>> islands) {
         List<GeneratedScript> scripts = new ArrayList<>();
+
         scripts.addAll(refactorClasses(clazz, islands));
+        // TODO Generate Table from new class not old one ...
         scripts.addAll(generateTables(clazz, islands));
         return scripts;
 
@@ -75,9 +73,10 @@ public class RelationalSQLBuilder extends ArchipelagoScriptBuilder {
         LOGGER.debug(String.format("generate UnderMetaclass %s [START]", clazz.getSimpleName()));
         classTemplatePath = TEMPLATE_ROOT_PATH + RELATIONAL_FOLDER + SQL_FOLDER + "\\Table.stg";
         group = StringTemplateFactory.buildSTGroup(classTemplatePath);
-        st = group.getInstanceOf("tableWrapper");
+        st = group.getInstanceOf("SQLTable");
         TableGeneratorWrapper table = scanTable(clazz);
         st.add("tableWrapper", table);
+        // TODO Pay attention to the comas for the table generated
         scripts.add(new GeneratedScript(String.format(CLASS_FILE_NAME_UNDER_META_FORMAT, clazz.getSimpleName()), st.render()));
         LOGGER.debug(String.format("generate UnderMetaclass %s [END]", clazz.getSimpleName()));
 
@@ -85,19 +84,26 @@ public class RelationalSQLBuilder extends ArchipelagoScriptBuilder {
 
     private TableGeneratorWrapper scanTable(Class<?> clazz) {
         List<SQLPropertyWrapper> properties = new ArrayList<>();
-        for (Field f : clazz.getFields()){
+        for (Field f : clazz.getDeclaredFields()) {
+            if (Collection.class.isAssignableFrom(f.getType())) {
+                // TODO List case
+                continue;
+            }
             JDBCType type = JDBCType.valueOf(JdbcTypeJavaClassMappings.INSTANCE.determineJdbcTypeCodeForJavaClass(f.getType()));
             Integer size = null;
-            switch (type){
+            switch (type) {
                 case CHAR:
                 case VARCHAR:
                 case NCHAR:
-                case NVARCHAR: size = 255;break;
-                default:break;
+                case NVARCHAR:
+                    size = 255;
+                    break;
+                default:
+                    break;
             }
             properties.add(new SQLPropertyWrapper(f.getName(), type.getName(), size));
         }
-        return new TableGeneratorWrapper(properties);
+        return new TableGeneratorWrapper(clazz.getSimpleName().toUpperCase(), properties);
     }
 
     private List<GeneratedScript> refactorClasses(Class<?> clazz, List<Class<?>> islands) {
@@ -120,7 +126,7 @@ public class RelationalSQLBuilder extends ArchipelagoScriptBuilder {
         STGroup group;
         ST st;
         LOGGER.debug(String.format("generate UnderMetaclass %s [START]", clazz.getSimpleName()));
-        classTemplatePath = TEMPLATE_ROOT_PATH + JAVA_FOLDER + "\\GarlandClass.stg";
+        classTemplatePath = TEMPLATE_ROOT_PATH + RELATIONAL_FOLDER + JAVA_FOLDER + "\\GarlandClass.stg";
         group = StringTemplateFactory.buildSTGroup(classTemplatePath);
         st = group.getInstanceOf("GarlandClass");
         st.add("className", clazz.getSimpleName());
@@ -137,21 +143,21 @@ public class RelationalSQLBuilder extends ArchipelagoScriptBuilder {
         STGroup group;
         ST st;
         LOGGER.debug(String.format("generate UnderMetaclass %s [START]", clazz.getSimpleName()));
-        classTemplatePath = TEMPLATE_ROOT_PATH + JAVA_FOLDER + "\\UnderMetaClass.stg";
+        classTemplatePath = TEMPLATE_ROOT_PATH + RELATIONAL_FOLDER + JAVA_FOLDER + "\\UnderMetaClass.stg";
         group = StringTemplateFactory.buildSTGroup(classTemplatePath);
         st = group.getInstanceOf("UnderMetaClass");
         st.add("className", clazz.getSimpleName());
         st.add("package", clazz.getPackage());
         for (Field field : clazz.getDeclaredFields()) {
             st.add("properties", field);
-        } 
+        }
         scripts.add(new GeneratedScript(String.format(CLASS_FILE_NAME_UNDER_META_FORMAT, clazz.getSimpleName()), st.render()));
         LOGGER.debug(String.format("generate UnderMetaclass %s [END]", clazz.getSimpleName()));
     }
 
     private void generateMetaClass(Class<?> clazz, List<GeneratedScript> scripts) {
         LOGGER.debug(String.format("generate Metaclass %s [START]", clazz.getSimpleName()));
-        String classTemplatePath = TEMPLATE_ROOT_PATH + JAVA_FOLDER + "\\MetaClass.stg";
+        String classTemplatePath = TEMPLATE_ROOT_PATH + RELATIONAL_FOLDER + JAVA_FOLDER + "\\MetaClass.stg";
         STGroup group = StringTemplateFactory.buildSTGroup(classTemplatePath);
         ST st = group.getInstanceOf("MetaClass");
         st.add("className", clazz.getSimpleName());
@@ -161,10 +167,12 @@ public class RelationalSQLBuilder extends ArchipelagoScriptBuilder {
     }
 
     private class TableGeneratorWrapper {
-        private List<SQLPropertyWrapper> fields;
+        private final String tableName;
+        private final List<SQLPropertyWrapper> fields;
 
-        public TableGeneratorWrapper(List<SQLPropertyWrapper> fields) {
+        public TableGeneratorWrapper(final String tableName, final List<SQLPropertyWrapper> fields) {
             super();
+            this.tableName = tableName;
             this.fields = fields;
         }
 
@@ -172,9 +180,10 @@ public class RelationalSQLBuilder extends ArchipelagoScriptBuilder {
             return fields;
         }
 
-        public void setFields(List<SQLPropertyWrapper> fields) {
-            this.fields = fields;
-        } 
+        public String getTableName() {
+            return tableName;
+        }
+
     }
 
     private class SQLPropertyWrapper {
@@ -214,6 +223,4 @@ public class RelationalSQLBuilder extends ArchipelagoScriptBuilder {
         }
 
     }
-}
-
 }
